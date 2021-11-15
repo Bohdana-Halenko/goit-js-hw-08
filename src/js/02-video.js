de2717816c5b0bad69159941f4eb09ab7831578c
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-11-15 22:30:48 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+const iframe = document.querySelector("#vimeo-player");
+const throttle = require("lodash.throttle");
+
+//подключение VimeoPlayer - npm
+const player = new Vimeo.Player("iframe");
+
+//сохранение текущего времени проигрования
 
