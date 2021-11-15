22d87816b3f370ab93b26b832b0fd79681b4a366
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-11-15 21:07:42 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// выводим gallery
+const galeryComponentRef = document.querySelector(".gallery");
+const addedGalleryCard = galleryCardsCreator(galleryItems);
+
+// functionCreator - создаем список изображений с элементами, полученных из galleryItems
+function galleryCardsCreator(gallery) {
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+// выводим галлерею на страницу
+
+galeryComponentRef.insertAdjacentHTML("beforeend", addedGalleryCard);
+
+// открытие лайтбокса
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function(){});
+
