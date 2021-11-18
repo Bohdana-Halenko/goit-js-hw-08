198eb7440da938748f2b21545d56903d693b8c6a
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-11-18 11:25:36 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,12 +6,13 @@ const player = new Vimeo.Player(iframe);
 
 //сохранение текущего времени проигрования
 function saveCurrentPlayerTime(date) {
-  localStorage.setItem('videoplayer-current-time', Math.round(date.seconds));
-  console.log(Number(localStorage.getItem('videoplayer-current-time')));
+  localStorage.setItem("videoplayer-current-time", Math.round(date.seconds));
+  console.log(Number(localStorage.getItem("videoplayer-current-time")));
 }
-player.on('timeupdate', throttle(saveCurrentPlayerTime, 1000));
+//время воспроизведения обновляется в хранилище раз в секунду
+player.on("timeupdate", throttle(saveCurrentPlayerTime, 1000));
 
 function setupCurrentPlayerTime() {
-  player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+  player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time")));
 }
 window.onload = setupCurrentPlayerTime;
