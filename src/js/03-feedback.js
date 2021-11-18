87dcc2223829b835cd89b27e1b0f0e11fad402f3
Bohdana-Halenko <danaermolenko20@gmail.com>
2021-11-18 16:00:15 +0200

    переписала

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,39 @@
 import throttle from 'lodash.throttle';
 
-const formREf = document.querySelector('.feedback-form');
-const emailREf = formREf.querySelector('input');
-const textREf = formREf.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[type="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 const STORAGE_KEY = 'feedback-form-state';
 
+//обновление хранилища каждые 500 миллисекунд
+form.addEventListener('input', throttle(localStorageFn, 500));
 
-function saveInputData(e) {
-  let feedbackFormState = {};
-  feedbackFormState[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+const dataObj = {};
+
+function localStorageFn(evt) {
+  dataObj[emailInput.name] = emailInput.value;
+  dataObj[messageInput.name] = messageInput.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
 }
 
-formREf.addEventListener('input', throttle(saveInputData, 500));
-formREf.addEventListener('submit', function (e) {
-  e.preventDefault();
-  e.currentTarget.reset();
-  console.log(localStorage.getItem(STORAGE_KEY));
+const savedData = localStorage.getItem(STORAGE_KEY);
+const parsedData = JSON.parse(savedData);
+
+form.addEventListener('submit', onFormClear);
+
+function onFormClear(evt) {
+  evt.preventDefault();
+  console.log(`${emailInput.name}:`, emailInput.value);
+  console.log(`${messageInput.name}:`, messageInput.value);
+  evt.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
-});
-
-function returnSevedMessage() {
-  let parstLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (parstLocalStorage) {
-    parstLocalStorage.email ? (emailREf.value = parstLocalStorage.email) : (emailREf.value = '');
-    parstLocalStorage.message ? (textREf.value = parstLocalStorage.message) : (textREf.value = '');
-  }
 }
 
-returnSevedMessage();
+function localStorageCheck() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+  }
+  return;
+}
+localStorageCheck();
